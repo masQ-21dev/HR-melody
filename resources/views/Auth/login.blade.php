085d2424838add32792c4ca5b41bc234bae9f33f
masQ-21dev <mqomarudin0480@gmail.com>
2023-07-19 19:11:35 +0700

    login blade

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'login')
 
 @section('content')
-  <div class="container">
+  {{-- <div class="container">
     <form action="{{ route('autahenticate')}}" method="POST">
         @csrf
       <!-- Email input -->
@@ -38,5 +38,187 @@
       <button type="submit" class="btn btn-primary btn-block mb-4">login</button>
 
     </form>
-  </div>
+  </div> --}}
+
+  <style>
+    .required-color {
+    color: #ff4747;
+    }
+    .error {
+        border-color: #ff4747;
+    }
+    .valid {
+        border-color: #37a137;
+    }
+    .hide {
+        display: none;
+    }
+    .field-icon {
+    float: right;
+    margin-left: -25px;
+    margin-top: -25px;
+    position: relative;
+    z-index: 2;
+    }
+</style>
+<main class="">
+    <section class="container">
+        <article class="row justify-content-center">
+            <div class="row justify-content-center">
+                <div class="col-md-6 text-center mb-4 mb-md-5 pt-5">
+                    <h2 class="heading-section" style="color: #1B016A; font-family: 'Poppins', sans-serif; font-weight: bold;">WELCOME TO HRIS PT. GATRA MAPAN</h2>
+                </div>
+            </div>
+            <div class="col-md-12 col-lg-6 pt-4 pt-md-5">
+                <div class="wrap d-md-flex justify-content-center shadow p-4 position-relative">
+                    <figure class="img w-50 mx-auto mx-md-0 pt-md-4 p-md-2">
+                        <img src="{{ asset('/') }}assets/images/Logo-Gatra.png" class="img-fluid position-relative" alt="">
+                    </figure>
+                    <div class="login-wrap p-2 p-md-4">
+                        <div class="d-flex">
+                            <div class="w-50">
+                                <h3 class="mb-4">Sign In</h3>
+                            </div>
+                    </div>
+                    <form class="signIn-form">
+                        <div class="input-section">
+                          <label class="mb-1">Username<span class="required-color">*</span></label>
+                          <input
+                            class="form-control"
+                            type="text"
+                            placeholder="Enter Username"
+                            id="username-input"
+                            required
+                          />
+                          <span id="username-error" class="hide required-color error-message"
+                            >Invalid Input</span
+                          >
+                          <span id="empty-username" class="hide required-color error-message"
+                            >This field is required.</span
+                          >
+                        </div>
+                        <div class="input-section mt-3">
+                          <label class="mb-1">Password<span class="required-color">*</span></label>
+                          <input
+                            class="form-control"
+                            type="password"
+                            placeholder="Enter Password"
+                            id="password"
+                            required
+                          />
+                          <span toggle="#password-field" class="fa fa-fw fa-eye-slash field-icon toggle-password"></span>
+                          <span id="password-error" class="hide required-color error-message"
+                            >Invalid Input</span
+                          >
+                          <span id="empty-password" class="hide required-color error-message"
+                            >Password required.</span
+                          >
+                        </div>
+                        <button class="form-control btn btn-primary rounded submit px-3 mt-3" id="submit-button">Log In</button>
+                        <label class="text-black-50 py-2 d-flex justify-content-center">--OR--</label>
+                        <button class="form-control btn btn-info rounded submit px-3" id="submit-button">Log In As Karyawan</button>
+                      </form>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
+</main>
+<script>
+    const togglePassword = document.querySelector('.toggle-password');
+    const pass = document.querySelector('#password');
+    togglePassword.addEventListener('click', function (e) {
+        // toggle the type attribute
+        const type = pass.getAttribute('type') === 'password' ? 'text' : 'password';
+        pass.setAttribute('type', type);
+        // toggle the eye / eye slash icon
+        this.classList.toggle('fa-eye');
+        this.classList.toggle('fa-eye-slash');
+    });
+
+    //Name
+    let usernameInput = document.getElementById("username-input");
+    let usernameError = document.getElementById("username-error");
+    let emptyUsernameError = document.getElementById("empty-username");
+
+    //Password
+    let password = document.getElementById("password");
+    let passwordError = document.getElementById("password-error");
+    let emptypasswordError = document.getElementById("empty-password");
+
+    //Submit
+    let submitButton = document.getElementById("submit-button");
+
+    //Valid
+    let validClasses = document.getElementsByClassName("valid");
+    let invalidClasses = document.getElementsByClassName("error");
+
+    const passwordVerify = (password) => {
+    const regex = /^[A-Za-z0-9!@#$%^&*()\-_=+[{\]}\\|;:'",<.>/?`~\s]{2,}$/;
+    return regex.test(password);
+    };
+
+
+//Text verification (if input contains only text)
+    const textVerify = (text) => {
+    const regex = /^[a-zA-Z]{3,}$/;
+    return regex.test(text);
+    };
+
+    //For empty input - accepts(input,empty error for that input and other errors)
+    const emptyUpdate = (
+    inputReference,
+    emptyErrorReference,
+    otherErrorReference
+    ) => {
+    if (!inputReference.value) {
+        //input is null/empty
+        emptyErrorReference.classList.remove("hide");
+        otherErrorReference.classList.add("hide");
+        inputReference.classList.add("error");
+    } else {
+        //input has some content
+        emptyErrorReference.classList.add("hide");
+    }
+    };
+
+    //For error styling and displaying error message
+    const errorUpdate = (inputReference, errorReference) => {
+    errorReference.classList.remove("hide");
+    inputReference.classList.remove("valid");
+    inputReference.classList.add("error");
+    };
+
+    //For no errors
+    const validInput = (inputReference) => {
+    inputReference.classList.remove("error");
+    inputReference.classList.add("valid");
+    };
+
+    //Name
+    usernameInput.addEventListener("input", () => {
+    if (textVerify(usernameInput.value)) {
+        //If verification returns true
+        usernameError.classList.add("hide");
+        validInput(usernameInput);
+    } else {
+        //for false
+        errorUpdate(usernameInput, usernameError);
+        //empty checker
+        emptyUpdate(usernameInput, emptyUsernameError, usernameError);
+    }
+    });
+
+    //Password
+    password.addEventListener("input", () => {
+    if (passwordVerify(password.value)) {
+        passwordError.classList.add("hide");
+        validInput(password);
+    } else {
+        errorUpdate(password, passwordError);
+        emptyUpdate(password, emptypasswordError, passwordError);
+    }
+    });
+
+</script>
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,14 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'HR manajement')</title>
+    {{-- bootstrap --}}
     <link href="{{ asset('/') }}assets/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    {{-- font google --}}
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('/') }}assets/plugins/fontawasome/css/fontawesome.min.css">
+
+
+
+
 </head>
 <body>
-  <h1 class="text-primary">Hello, world!</h1>
-  <main class="container">
-      @yield('content')
-  </main>
 
-  <script src="{{ asset('/') }}assets/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    @yield('content')
+
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="{{ asset('/') }}assets/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
